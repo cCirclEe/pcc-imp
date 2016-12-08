dfbe2de24d8c09a1f8985a6b99a6bdaeb4e29832
Giorgio Groß <uvech@student.kit.edu>
2016-12-08 15:45:57 +0100

    Initial commit; Implement first design and app structure

---
+++ PrivacyCrashCam/app/src/main/java/de/pcc/privacycrashcam/CameraView.java
@@ -0,0 +1,38 @@
+package de.pcc.privacycrashcam;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Created by Giorgio on 08.12.16.
+ */
+
+public class CameraView extends SurfaceView implements SurfaceHolder.Callback, Camera.PreviewCallback {
+
+    public CameraView(Context context, AttributeSet attrs) {
+        super(context);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void onPreviewFrame(byte[] data, Camera camera) {
+
+    }
+}
